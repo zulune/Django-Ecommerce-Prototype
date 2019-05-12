d7fe8f093e78f945d7f6af8e872788817f40fcea
zulune <mars.xaker@gmail.com>
2019-05-12 20:04:10 +0300

    Viewed product attribute

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Item, Order, OrderItem
 # Register your models here.
 
-admin.site.register(Item)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+    
+
 admin.site.register(Order)
 admin.site.register(OrderItem)
