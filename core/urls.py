6c5c8a5963526782d9f8623fcc5dd69c5e78deb6
zulune <mars.xaker@gmail.com>
2019-05-12 16:26:16 +0300

    Add free bootstrap theme

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import item_list, checkout, products
+
+
+app_name = 'core'
+
+
+urlpatterns = [
+    path('', item_list, name='item-list'),
+    path('checkout/', checkout, name='checkout'),
+    path('products/', products, name='products'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
+
+def item_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, 'home-page.html', context)
+
+
+def checkout(request):
+    return render(request, 'checkout-page.html', {})
+
+
+def products(request):
+    return render(request, 'product-page.html', {})
